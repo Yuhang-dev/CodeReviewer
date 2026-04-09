0bd3ca88d9edde32aeb8bef2a2ff7573b1b43d5e
Yuhang-Notebook <yuhangchengdev@gmail.com>
2026-04-09 22:08:20 +0800

    Test Line Comment

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from app.core.config import settings
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def create_app() -> FastAPI:
+def create_app():
     app = FastAPI(title=settings.PROJECT_NAME)
 
     app.add_middleware(
